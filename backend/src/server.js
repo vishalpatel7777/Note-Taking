18ea54196488f4cdcd9a6b9340f214911887518a
vishalpatel7777 <patelvishal14642@gmail.com>
2025-06-20 15:18:38 +0530

    commit before deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,23 +2,27 @@ import express from "express"; // it give me warning
 // const express = require('express');
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import notesRoutes from "./routes/noteRoutes.js";
 import ratelimiter from "./middleware/rateLimiter.js";
-
-
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 
-app.use(cors({
-  origin: "http://localhost:5173",}
-)); 
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
 // this is middleware basicallt parse the json bodies : req.body
 app.use(express.json());
-
 
 // simple custom middleware
 // app.use((req, res, next) => {
@@ -26,10 +30,16 @@ app.use(express.json());
 //   next();
 // });
 
-
 app.use(ratelimiter);
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
